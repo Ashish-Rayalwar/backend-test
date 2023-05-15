1381b100c641c76b8ed619c9e5d80a5a409eab56
Ashish Rayalwar <rayalwar.ashish.1@gmail.com>
2023-05-15 13:28:00 +0530

    first commit9

--- src/database/db.js
+++ src/database/db.js
@@ -4,12 +4,20 @@ const password = process.env.PASSWORD;
 const host = process.env.HOST;
 const root = process.env.ROOT;
 const dbName = process.env.DBNAME;
-const db = sql.createConnection({
-  host: host,
-  user: root,
-  password: password,
-  database: dbName,
+// const db = sql.createConnection({
+//   host: host,
+//   database: dbName,
+//   user: root,
+//   password: password,
+// });
+
+let db = sql.createConnection({
+  host: process.env.MYSQL_ADDON_HOST,
+  database: process.env.MYSQL_ADDON_DB,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
 });
+
 // const db = sql.createConnection({
 //   host: host,
 //   user: "root",
