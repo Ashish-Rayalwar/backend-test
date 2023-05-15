17aa2d68268e57e1299f8d0d03f3091df0695d85
Ashish Rayalwar <rayalwar.ashish.1@gmail.com>
2023-05-15 12:48:45 +0530

    first commit6

--- src/database/db.js
+++ src/database/db.js
@@ -1,12 +1,21 @@
 const sql = require("mysql");
 require("dotenv").config();
 const password = process.env.PASSWORD;
+const host = process.env.HOST;
+const root = process.env.ROOT;
+const dburl = process.env.DBURL;
 const db = sql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password12345",
-  database: "blog",
+  host: host,
+  user: root,
+  password: password,
+  database: dburl,
 });
+// const db = sql.createConnection({
+//   host: host,
+//   user: "root",
+//   password: "password12345",
+//   database: "blog",
+// });
 
 db.connect((err) => {
   if (err) {
